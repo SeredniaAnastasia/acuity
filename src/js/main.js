6faba09cff468204ccdb1721ba3422ee0ad39db5
Serednia Anastasia <anastastasiaseredna.gmail.com>
2024-01-13 17:36:45 +0100

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,21 @@
+import $ from "jquery";
+import 'slick-slider';
 import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
 import './functions/burger';
+
+
+
+$('.variable-width').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
