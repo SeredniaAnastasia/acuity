8c42f0989088e9630cf4a50a6d8099808a0e27e8
Serednia Anastasia <anastastasiaseredna.gmail.com>
2024-01-18 18:11:39 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ __webpack_require__.r(__webpack_exports__);
 console.log('auto');
 function autoHeight(parentElementsClass) {
   let parentElements = document.querySelector("." + parentElementsClass);
-  const elements = document.querySelectorAll("." + parentElementsClass + " > li");
+  const elements = document.querySelectorAll("." + parentElementsClass + " > *");
   const counter = elements.length;
   const counterHalf = Math.ceil(counter / 2);
   let heightBlock = 0;
@@ -165,9 +165,15 @@ function autoHeight(parentElementsClass) {
       if (heightLeft === heightRight) {
         heightBlock += heightLeft;
       } else {
-        heightBlock += Math.max(heightLeft, heightRight);
-        elements[i].style.height = Math.max(heightLeft, heightRight) + "px";
-        elements[i + counterHalf].style.height = Math.max(heightLeft, heightRight) + "px";
+        // heightBlock += Math.max(heightLeft, heightRight)
+        // elements[i].style.height = Math.max(heightLeft, heightRight)+"px"
+        // elements[i+counterHalf].style.height = Math.max(heightLeft, heightRight) + "px"
+
+        if (heightLeft > heightRight) {
+          elements[i + counterHalf].style.height = heightLeft + "px";
+        } else {
+          elements[i].style.height = heightRight + "px";
+        }
       }
     } else {
       heightBlock += heightLeft;
@@ -176,6 +182,40 @@ function autoHeight(parentElementsClass) {
   parentElements && (parentElements.style.height = heightBlock + 15 + "px");
 }
 autoHeight("footer__links-list");
+
+// setInterval(()=>{
+//     // Створити новий елемент li
+//     var newLi = document.createElement('li');
+//     newLi.className = 'footer__links-item'; // Додаємо клас
+
+//     // Створити новий елемент a
+//     var newLink = document.createElement('a');
+//     newLink.className = 'footer__links-link'; // Додаємо клас
+//     newLink.href = '#'; // Встановлюємо атрибут href
+//     newLink.textContent = 'Help & Support'; // Встановлюємо текст внутрішнього вмісту
+
+//     // Додаємо елемент a як дочірній для елемента li
+//     newLi.appendChild(newLink);
+
+//     // Додаємо елемент li до потрібного батьківського елемента в DOM
+//     // Наприклад, можливо, у вас є вже існуючий список ul з класом "footer__links-list"
+//     var parentUl = document.querySelector('.footer__links-list');
+//     parentUl.appendChild(newLi);
+
+// },5000)
+
+// let parentElements1 = document.getElementsByClassName('footer__links-item').length
+// console.log(parentElements1)
+// setInterval(()=>{
+//     let parentElements2 = document.getElementsByClassName('footer__links-item').length
+//     console.log(parentElements2)
+//     if(!(parentElements1===parentElements2)){
+//     console.log('parentElements2*********')
+
+//         parentElements1 = parentElements2
+//         autoHeight("footer__links-list");
+//     }
+// }, 500)
 
 /***/ }),
 
@@ -14528,7 +14568,10 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-jquery__WEBPACK_IMPORTED_MODULE_0___default()('.variable-width').slick({
+// "slick-slider": "^1.8.2",
+// import fancyboxaa from "../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js";
+
+jquery__WEBPACK_IMPORTED_MODULE_0___default()('variable-width').slick({
   dots: false,
   arrows: false,
   infinite: true,
